54a0d615fd2de361fc0aa9256f3ac6512c6a0692
Itzalem <melissa.lj04@gmail.com>
2026-02-12 22:57:15 +0100

    basic components and models for history

---
+++ app/Views/History/HistoryHomepage.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Views\Home\History;
+
+?>
+
+<section class="flex flex-col gap-6 bg_colors_home text_colors_home pt-4">
+    <?php include 'Components/HistoryHero.php'; ?>
+    <?php include 'Components/HistoryWelcome.php'; ?>
+    <?php include 'Components/HistoryMainLandmarks.php'; ?>
+    <?php include 'Components/HistoryBookTour.php'; ?>
+</section>
