3eceeef2aebe9b92b782b944d90c8747134d9b29
Hunter M. Pandt <726523@student.inholland.nl>
2026-03-31 20:13:12 +0200

    Update image sources for YummyEventsSection

--- app/Views/Yummy/Components/YummyEventsSection.php
+++ app/Views/Yummy/Components/YummyEventsSection.php
@@ -10,7 +10,7 @@ namespace App\Views\Yummy\Components;
       <div class="grid md:grid-cols-4 gap-6 max-w-6xl mx-auto">
         <div class="bg-white rounded shadow text-center">
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/b/b8/Fireworks_in_Haarlem.jpg"
+            src="Assets/Jazz/JazzHome/Haarlem-Homepage.webp"
             class="rounded-t"
             alt="Jazz Festival"
           />
@@ -25,7 +25,7 @@ namespace App\Views\Yummy\Components;
         </div>
         <div class="bg-white rounded shadow text-center">
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Theatre_in_Haarlem.jpg"
+            src="Assets/Magic/AltHeroImage.png"
             class="rounded-t"
             alt="Magic Festival"
           />
@@ -40,13 +40,13 @@ namespace App\Views\Yummy\Components;
         </div>
         <div class="bg-white rounded shadow text-center">
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/d/dd/Haarlem_Church.jpg"
+            src="Assets/History/History_Church_1.png"
             class="rounded-t"
             alt="History Festival"
           />
           <div class="p-4">
             <h3 class="font-semibold">History - Haarlem Festival</h3>
-            <button
+            <button 
               class="bg-[var(--yummy-sec-btn)] text-[var(--yummy-sec-btn-text)] hover:bg-[var(--yummy-sec-hover-btn)] hover:text-[var(--yummy-sec-hover-btn-text)] border border-[var(--yummy-sec-section)] text-white text-sm px-4 py-2 rounded mt-3"
             >
               View more
@@ -55,7 +55,7 @@ namespace App\Views\Yummy\Components;
         </div>
         <div class="bg-white rounded shadow text-center">
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/9/93/Dance_event_Haarlem.jpg"
+            src="Assets/Dance/DanceHome/69a41a0ad1451_1772362250.webp"
             class="rounded-t"
             alt="Dance Festival"
           />
